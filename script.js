99427f66efef89101c42798a40af940eac5c98a4
islemAZ360 <islemmamidoslema@gmail.com>
2026-09-16 03:11:18 +0300

    feat: implement cookie storage manager and student table view (fixes #8, #7)

---
+++ script.js
@@ -0,0 +1,64 @@
+function displayStudent() {
+
+    const tableBody = document.getElementById("student-body");
+    tableBody.innerHTML = '' ;
+
+    let studentsList = getStudentsFromCookie();
+
+
+    if (studentsList.length === 0){
+        studentsList = [
+        { fullName: "Islam Azaizia", group: "P3224", isuNumber: 470449, roomNumber: 2224 },
+        { fullName: "bomba lakaka", group: "P3224", isuNumber: 654321, roomNumber: 505 }
+    ];
+    }
+
+
+    const studentsAsText = JSON.stringify(studentsList);
+
+    document.cookie = "students=" + studentsAsText;
+ 
+    
+
+    
+
+    studentsList.forEach( student => {
+
+    tableBody.innerHTML += `
+        <tr>
+            <td>${student.fullName}</td>
+            <td>${student.group}</td>
+            <td>${student.isuNumber}</td>
+            <td>${student.roomNumber}</td>
+            <td>
+                <button>View</button>
+                <button>Edit</button>
+                <button>Delete</button>
+            </td>
+        </tr>
+    `;
+    });
+}
+
+function getStudentsFromCookie() {
+    
+    const allCookies = document.cookie;
+    const cookiesArray = allCookies.split(";")
+
+    for (let cookie of cookiesArray){
+        let [name, valu] = cookie.trim().split("=");
+
+        if (name === "students")
+            return JSON.parse(valu);
+
+    } return [];
+
+
+
+
+
+
+}
+
+
+displayStudent();
